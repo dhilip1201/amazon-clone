5116b549bad3e4782faaa26e76be39295a911394
dhilip1201 <dhilipbeece006@gmail.com>
2020-09-17 16:08:58 +0530

    Updates

---
+++ precache-manifest.9430a5def898b584c6b42892d819784b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f0765f9e042d181c08d6b7881b8db1",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "f58ac7ef775f0911f2fd",
+    "url": "/amazon-clone/static/css/main.af1e554e.chunk.css"
+  },
+  {
+    "revision": "9071e80df2c18330de24",
+    "url": "/amazon-clone/static/js/2.c7bc7a59.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.c7bc7a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58ac7ef775f0911f2fd",
+    "url": "/amazon-clone/static/js/main.7f00342f.chunk.js"
+  },
+  {
+    "revision": "1b8ce24b6f2e40dac959",
+    "url": "/amazon-clone/static/js/runtime-main.b6fb7875.js"
+  }
+]);
